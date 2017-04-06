be2f451be698db324d9b8c4e66031b32a19ab6ec
Nuno Matos <nunobarros_@hotmail.com>
2017-04-06 03:20:10 +0100

    removing import

--- src/main/java/org/feup/lgp2d/helpwin/dao/repositories/AbstractRepository.java
+++ src/main/java/org/feup/lgp2d/helpwin/dao/repositories/AbstractRepository.java
@@ -9,7 +9,6 @@ import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public abstract class AbstractRepository<T> {
 
