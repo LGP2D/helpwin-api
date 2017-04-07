22974fff9a705336c63bfe6d8a01625a7fa6613c
Nuno Matos <nunobarros_@hotmail.com>
2017-04-07 15:23:34 +0100

    populate. route endpoint controller.

--- src/main/java/org/feup/lgp2d/helpwin/endpoints/RoleController.java
+++ src/main/java/org/feup/lgp2d/helpwin/endpoints/RoleController.java
@@ -1,20 +1,23 @@
-package org.feup.lgp2d.helpwin;
+package org.feup.lgp2d.helpwin.endpoints;
 
-import com.sun.org.apache.regexp.internal.RESyntaxException;
 import org.feup.lgp2d.helpwin.dao.repositories.repositoryImplementations.RoleRepository;
 import org.feup.lgp2d.helpwin.models.Role;
 
-import javax.annotation.PreDestroy;
 import javax.annotation.security.PermitAll;
-
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("roles")
-public class Resource {
+public class RoleController {
 
+    /**
+     * Get all roles
+     * @return Response - returns a response with the roles embedded in the response package
+     *                    or an error if something went wrong.
+     */
     @PermitAll
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -28,9 +31,15 @@ public class Resource {
         }
     }
 
+    /**
+     * Get the role given the id
+     * @param id the id of the role to be returned
+     * @return Response - returns a response with the role embedded in the response package
+     *                    or an error if something went wrong.
+     */
     @PermitAll
     @GET
-    @Path("roles/{id}")
+    @Path("/{id}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
     public Response getRoleById(@PathParam("id")int id) {
         RoleRepository roleRepository = new RoleRepository();
@@ -42,9 +51,13 @@ public class Resource {
         }
     }
 
+    /**
+     * Creates a role
+     * @param role - the model of the role to be created
+     * @return Response - returns the model of the created role or an error if something went wrong
+     */
     @PermitAll
     @POST
-    @Path("roles")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createRole(Role role) {
@@ -57,9 +70,13 @@ public class Resource {
         }
     }
 
+    /**
+     * Updates a role
+     * @param role - the model of the role to be updated
+     * @return Response - returns the role updated or an error if something went wrong
+     */
     @PermitAll
     @PUT
-    @Path("roles")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateRole(Role role) {
@@ -72,9 +89,14 @@ public class Resource {
         }
     }
 
+    /**
+     * Deletes a role
+     * @param id the id of the role to be deleted
+     * @return Response - an Ok response if everything went successful, or an error if something went wrong
+     */
     @PermitAll
     @DELETE
-    @Path("roles/{id}")
+    @Path("/{id}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
     public Response deleteRoleById(@PathParam("id")int id) {
         RoleRepository roleRepository = new RoleRepository();
