07aba185bf26a6c372990b1258872f54782c54f8
rolando <rolando.a@sapo.pt>
2017-04-24 22:04:16 +0100

    Institution model , endpoint

---
+++ src/main/java/org/feup/lgp2d/helpwin/endPoint/InstitutionController.java
@@ -0,0 +1,7 @@
+package org.feup.lgp2d.helpwin.endPoint;
+
+/**
+ * Created by HP on 24/04/2017.
+ */
+public class InstitutionController {
+}
---
+++ src/main/java/org/feup/lgp2d/helpwin/models/Institution.java
@@ -0,0 +1,7 @@
+package org.feup.lgp2d.helpwin.models;
+
+/**
+ * Created by HP on 24/04/2017.
+ */
+public class Institution {
+}
