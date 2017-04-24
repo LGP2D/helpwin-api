9b25cb3801d97af8b528ac6fe95527d87a5ba179
rolando <rolando.a@sapo.pt>
2017-04-24 22:05:34 +0100

    Institution model and endpoint

--- src/main/java/org/feup/lgp2d/helpwin/dao/repositories/repositoryImplementations/InstitutionRepository.java
+++ src/main/java/org/feup/lgp2d/helpwin/dao/repositories/repositoryImplementations/InstitutionRepository.java
@@ -1,7 +1,12 @@
 package org.feup.lgp2d.helpwin.dao.repositories.repositoryImplementations;
 
-/**
- * Created by HP on 24/04/2017.
- */
-public class InstitutionRepository {
+import org.feup.lgp2d.helpwin.dao.repositories.AbstractRepository;
+import org.feup.lgp2d.helpwin.models.Institution;
+
+public class InstitutionRepository extends AbstractRepository<Institution> {
+    @Override
+    public Class<Institution> getDomainClass() {
+        return Institution.class;
+    }
 }
+
