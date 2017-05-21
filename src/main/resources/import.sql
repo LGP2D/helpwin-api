82e2b06e9ae494d8032fd7cd67dd9b2ae4bbad7a
João Gomes <jlmgomes77@gmail.com>
2017-05-21 19:32:00 +0100

    no message

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,7 +9,7 @@ INSERT INTO institutions(description,imageUrl,name) values("SEM ABRIGOS","http:/
 #INSERT INTO actions(description,endDate,startDate,type,valid,institution_id) values("Action A","2017-08-01","2017-08-01","tipo test",true,1);
 #INSERT INTO actions(description,endDate,startDate,type,valid,institution_id,verified,availablePosition) values("Action A","2017-08-01","2017-08-01","tipo test",true,1,false,10);
 
-INSERT INTO vouchers (description, imagePath, company) VALUES ("voucher", "http://voxsingingacademy.com.au/wp-content/uploads/2012/11/gift-voucher.png", "fnac");
-INSERT INTO vouchers (description, imagePath, company) VALUES ("voucher", "http://www.carloisles.com/project-cms/wp-content/uploads/2014/10/Takatack-Free-150-Shopping-Voucher-Coupon.png", "worten");
-INSERT INTO vouchers (description, imagePath, company) VALUES ("voucher", "http://www.sonorous.tv/product_images/uploaded_images/gift-voucher.png", "adidas");
-INSERT INTO vouchers (description, imagePath, company) VALUES ("voucher", "http://www.wordmstemplates.com/wp-content/uploads/2015/08/gift-voucher-template-1245.jpg", "nike");
+INSERT INTO vouchers (description, imagePath) VALUES ("voucher", "http://voxsingingacademy.com.au/wp-content/uploads/2012/11/gift-voucher.png");
+INSERT INTO vouchers (description, imagePath) VALUES ("voucher", "http://www.carloisles.com/project-cms/wp-content/uploads/2014/10/Takatack-Free-150-Shopping-Voucher-Coupon.png");
+INSERT INTO vouchers (description, imagePath) VALUES ("voucher", "http://www.sonorous.tv/product_images/uploaded_images/gift-voucher.png");
+INSERT INTO vouchers (description, imagePath) VALUES ("voucher", "http://www.wordmstemplates.com/wp-content/uploads/2015/08/gift-voucher-template-1245.jpg");
